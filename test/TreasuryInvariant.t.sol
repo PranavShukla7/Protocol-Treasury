b756d337b51042d188dca96a11cfe59548ec6a94
PranavShukla7 <shuklapranav789@gmail.com>
2026-09-25 17:03:44 +0530

    invariant-using valid address target functions to revert

--- test/TreasuryInvariant.t.sol
+++ test/TreasuryInvariant.t.sol
@@ -1,74 +1,91 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.35;
 
 import {Test} from "forge-std/Test.sol";
 import {Treasury} from "../src/Treasury.sol";
 
 contract Handler is Test {
     Treasury public treasury;
-    address public depositor;
+    address public ownerOne;
+    address public ownerTwo;
 
-    constructor(Treasury _treasury, address _depositor) {
+    constructor(Treasury _treasury, address _ownerOne, address _ownerTwo) {
         treasury = _treasury;
-        depositor = _depositor;
+        ownerOne = _ownerOne;
+        ownerTwo = _ownerTwo;
     }
 
-    function deposit(uint96 rawAmount) public {
-        uint256 amount = bound(rawAmount, 1, 100 ether);
+    function _actor(bool useOwnerTwo) private view returns (address) {
+        return useOwnerTwo ? ownerTwo : ownerOne;
+    }
 
-        vm.deal(depositor, amount);
-        vm.prank(depositor);
+    function deposit(uint96 rawAmount, bool useOwnerTwo) public {
+        uint256 amount = bound(rawAmount, 1, 100 ether);
+        address actor = _actor(useOwnerTwo);
+
+        vm.deal(actor, amount);
+        vm.prank(actor);
         treasury.deposit{value: amount}();
     }
 
-    function submitTransaction(address to, uint256 value) public {
-        vm.prank(depositor);
-        treasury.submitTransaction(to, value);
+    function submitTransaction(address recipient, uint96 rawAmount, bool useOwnerTwo) public {
+        uint256 amount = bound(rawAmount, 1, 100 ether);
+
+        vm.prank(_actor(useOwnerTwo));
+        treasury.submitTransaction(recipient, amount);
     }
 
-    function approveTransaction(uint256 transactionIndex) public {
-        vm.prank(depositor);
+    function approveTransaction(uint256 transactionIndex, bool useOwnerTwo) public {
+        vm.prank(_actor(useOwnerTwo));
         treasury.approveTransaction(transactionIndex);
     }
 
-    function queueTransaction(uint256 transactionIndex) public {
-        vm.prank(depositor);
+    function queueTransaction(uint256 transactionIndex, bool useOwnerTwo) public {
+        vm.prank(_actor(useOwnerTwo));
         treasury.queue(transactionIndex);
     }
 
-    function cancelTransaction(uint256 transactionIndex) public {
-        vm.prank(depositor);
+    function cancelTransaction(uint256 transactionIndex, bool useOwnerTwo) public {
+        vm.prank(_actor(useOwnerTwo));
         treasury.cancelTransaction(transactionIndex);
     }
 
-    function executeTransaction(uint256 transactionIndex) public {
-        vm.prank(depositor);
+    function executeTransaction(uint256 transactionIndex, bool useOwnerTwo) public {
+        vm.prank(_actor(useOwnerTwo));
         treasury.execute(transactionIndex);
     }
 
-    function pause() public {
-        vm.prank(depositor);
+    function pause(bool useOwnerTwo) public {
+        vm.prank(_actor(useOwnerTwo));
         treasury.pause();
     }
 
-    function unpause() public {
-        vm.prank(depositor);
+    function unpause(bool useOwnerTwo) public {
+        vm.prank(_actor(useOwnerTwo));
         treasury.unpause();
+    }
+
+    function advanceTime(uint32 rawDelay) public {
+        uint256 delay = bound(rawDelay, 0, 2 days);
+        vm.warp(block.timestamp + delay);
     }
 }
 
 contract TreasuryInvariantTest is Test {
     Treasury public treasury;
     Handler public handler;
-    address public depositor;
+    address public ownerTwo = address(0xDEAD);
 
     function setUp() public {
-        depositor = address(0xDEAD);
         treasury = new Treasury();
-        handler = new Handler(treasury, depositor);
+        treasury.addOwner(ownerTwo);
 
-        treasury.addOwner(depositor);
-        //treasury.addOwner(address(this));
+        _executeRoleChange(ownerTwo, Treasury.Role.Guardian, true);
+        _executeRoleChange(ownerTwo, Treasury.Role.Executor, true);
+        _executeRoleChange(ownerTwo, Treasury.Role.Treasurer, true);
+
+        handler = new Handler(treasury, address(this), ownerTwo);
+        targetContract(address(handler));
     }
 
     function invariantTransactionsHaveValidState() public view {
@@ -112,5 +129,17 @@ contract TreasuryInvariantTest is Test {
 
             assertEq(confirmations, approvalCount);
         }
+    }
+
+    function _executeRoleChange(address account, Treasury.Role role, bool grant) private {
+        uint256 roleChangeIndex = treasury.proposeRoleChange(account, role, grant);
+        treasury.approveRoleChange(roleChangeIndex);
+
+        vm.prank(ownerTwo);
+        treasury.approveRoleChange(roleChangeIndex);
+
+        treasury.queueRoleChange(roleChangeIndex);
+        vm.warp(block.timestamp + treasury.ROLE_CHANGE_DELAY());
+        treasury.executeRoleChange(roleChangeIndex);
     }
 }
